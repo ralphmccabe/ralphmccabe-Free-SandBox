639ede30f5c678ba90cb5f33c416d8d628916f0d
ralphmccabe <44793443+ralphmccabe@users.noreply.github.com>
2026-05-20 16:31:17 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* TRC-PRO-VERSION - v2.14-PROD */
-const CACHE_NAME = 'trc-pro-upgrade-v2.14-PROD';
+/* TRC-PRO-VERSION - v2.14.2-PROD */
+const CACHE_NAME = 'trc-pro-upgrade-v2.14.2-PROD';
 const ASSETS = [
     './',
     './index.html?v=6.1',
