0aca734b9a98659375dde40a390dece5103d439e
ralphmccabe <44793443+ralphmccabe@users.noreply.github.com>
2026-05-24 21:01:37 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* TRC-VERSION - v3.0.19 */
-const CACHE_NAME = 'trc-v3.0.19';
+/* TRC-VERSION - v3.0.20 */
+const CACHE_NAME = 'trc-v3.0.20';
 const ASSETS = [
     './',
     './index.html?v=6.1',
@@ -92,6 +92,7 @@ self.addEventListener('fetch', event => {
         })
     );
 });
+
 
 
 
